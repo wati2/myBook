0aa93a2dfcf53c3672e1f51ae880a91abc2f4647
wati2 <yt.wati@gmail.com>
2020-05-19 19:55:05 +0900

    **html파일에 변수전달 부분

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
-
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the posts index.")
+    # return HttpResponse("Hello, world. You're at the posts index.")
+    template = loader.get_template('posts/index.html')
+    return HttpResponse(template.render({'vPass':'전달된 값 헬로우'},request))
+
